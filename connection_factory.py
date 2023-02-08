941247625104f48ce2cec4e7d8c1a7e9256e5729
Caroline Ferraz <projetos.crrz@gmail.com>
2023-02-08 13:28:35 -0300

    modificando connection_factory para se conectar com servidor de banco de dados da azure

--- connection_factory.py
+++ connection_factory.py
@@ -1,9 +1,7 @@
 import pyodbc
 
 conn = pyodbc.connect(
-    "Driver={SQL Server};"
-    "Server=DESKTOP-62M87GL\SQLEXPRESS;"
-    "Database=PythonSQL;"
+    "Driver={ODBC Driver 18 for SQL Server};Server=tcp:primeiro-data-lake.database.windows.net,1433;Database=python-api;Uid=administrador;Pwd=#r00t123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 )
 
 cursor = conn.cursor()
